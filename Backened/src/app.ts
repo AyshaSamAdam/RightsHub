3af5f779be5efcb5f4120f74bf963c74e30c91b8
AyshaSamAdam <ayesha.btx09@gmail.com>
2026-07-22 13:47:07 -0400

    Fix: CORS origin env Variable

--- Backened/src/app.ts
+++ Backened/src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 //  MIDDLEWARES 
 app.use(helmet())
 app.use(cors({
-     origin : 'http://localhost:5173',
+     origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
