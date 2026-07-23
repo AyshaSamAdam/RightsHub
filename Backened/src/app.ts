be043824630a8919e978f6e98353270bc8bdedfc
AyshaSamAdam <ayesha.btx09@gmail.com>
2026-07-23 09:41:58 -0400

    add cors fixed

--- Backened/src/app.ts
+++ Backened/src/app.ts
@@ -12,9 +12,12 @@ const app = express()
 //  MIDDLEWARES 
 app.use(helmet())
 app.use(cors({
-     origin : process.env.FRONTEND_URL,
-    credentials : true
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
+app.options('*', cors())
 app.use(express.json())
 app.use(cookieParser())
 
--- Backened/src/controllers/user.controllers.ts
+++ Backened/src/controllers/user.controllers.ts
@@ -210,7 +210,7 @@ async function loginController(req: Request<{}, {}, LoginRequestBody>, res: Resp
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
